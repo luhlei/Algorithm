297cabbae2455a8abffd0ff7f8ef72352d5aa2ab
陆浩磊 <99170605+luhlei@users.noreply.github.com>
2022-06-01 11:20:17 +0800

    字符串的全排列

---
+++ StructureAlgorithm/src/LeetCode/剑指Offer38字符串的排列.java
@@ -0,0 +1,32 @@
+package LeetCode;
+
+import java.util.HashSet;
+
+public class 剑指Offer38字符串的排列 {
+    HashSet<String> res=new HashSet<>();
+
+    public String[] permutation(String s) {
+        char[] chars = s.toCharArray();
+        boolean[] visited=new boolean[s.length()];
+        StringBuffer sb=new StringBuffer();
+        help(chars,sb,visited);
+        return res.toArray(String[]::new);
+    }
+    public void help(char[] chars,StringBuffer sb,boolean[] visited){
+        if (sb.length()==chars.length){
+            String s = sb.toString();
+            res.add(s);
+            return;
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (visited[i]){
+                continue;
+            }
+            sb.append(chars[i]);
+            visited[i]=true;
+            help(chars,sb,visited);
+            sb.deleteCharAt(sb.length()-1);
+            visited[i]=false;
+        }
+    }
+}
