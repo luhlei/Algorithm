2f8d9afdf8593076b51e4be77e3fc6f499ae6316
陆浩磊 <99170605+luhlei@users.noreply.github.com>
2022-06-10 21:32:01 +0800

    lc

---
+++ StructureAlgorithm/src/LeetCode/lc54.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+public class lc54 {
+    int value;
+    int cont;
+    public int kthLargest(TreeNode root, int k) {
+        traverse(root,k);
+        return value;
+    }
+    public void traverse(TreeNode root,int k){
+        if(root==null){
+            return;
+        }
+        traverse(root.right,k);
+        cont++;//注意java是值传递的，所以不能通过修改k，来实现
+        if (cont==k) value=root.val;
+        traverse(root.left,k);
+    }
+}
