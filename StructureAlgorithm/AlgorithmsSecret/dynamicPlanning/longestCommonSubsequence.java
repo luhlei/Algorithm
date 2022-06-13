43b810cfcb35d731370f281963f8e43c33854ce9
陆浩磊 <99170605+luhlei@users.noreply.github.com>
2022-06-13 11:42:32 +0800

    动态规划-最长公共子序列

---
+++ StructureAlgorithm/AlgorithmsSecret/dynamicPlanning/longestCommonSubsequence.java
@@ -0,0 +1,28 @@
+package AlgorithmsSecret.dynamicPlanning;
+
+import org.junit.Test;
+
+
+public class longestCommonSubsequence {
+    public int longestSubsequence(String str1,String str2){
+        int m=str1.length(),n=str2.length();
+        if (m==0||n==0) return 0;
+        int[][] dp=new int[m+1][n+1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(i-1)==str2.charAt(j-1)){//注意String的脚标，不能越界
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    @Test
+    public void test(){
+        String s1="babcde";
+        String s2="acze";
+        System.out.println(longestSubsequence(s1, s2));
+    }
+}
